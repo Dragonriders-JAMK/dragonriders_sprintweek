fa9b729d6d04a893819f123c0a308ea30e40131d
Maurnyy <124340374+Maurnyy@users.noreply.github.com>
2024-05-14 14:51:12 +0300

    Update list.component.ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { KarttaService } from '../kartta.service';
+import { Kartta } from '../kartta';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
 })
 export class ListComponent {
+  karttas: Array<Kartta> = []; // opiskelijat tulevat tähän taulukkoon
 
+  /* konstruktorissa injektoidaan (DI) KarttaService
+tähän komponenttiin.*/
+  constructor(private KarttaService: KarttaService) {
+    this.KarttaService.getKarttas().subscribe((data) => (this.karttas = data));
+  }
 }
