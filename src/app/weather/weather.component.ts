eaf327f25b32c3c915bbc71cf90df06b37553360
Jaakko Nojonen <ac9194@student.jamk.fi>
2024-05-15 10:19:29 +0300

    säähommeli kokeilu,

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Weather } from '../weather';
+//import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
-
+  weather: Weather = {
+    city: 'Jyväskylä',
+    conditions: 'Sunny',
+    temperature: 20,
+    icon: '',
+  };
 }
